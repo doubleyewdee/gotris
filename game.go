4f0beb629cda93bc5cf9c8abf1ec9b9eb3771d2c
Chip Locke <wd@teleri.net>
2020-04-12 22:49:53 -0700

    I can flip a board's colors back and forth

--- game.go
+++ game.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 	"unicode"
 
@@ -23,16 +24,35 @@ func NewGame(screen tcell.Screen) Game {
 }
 
 func (game *Game) Run() {
-	initScreen(game.screen)
+	game.screen.Init()
+	game.screen.Clear()
 	defer game.screen.Fini()
+
 	go game.getInput()
 
+	drawCount := 0
 	for {
+		game.writeMsg("awaiting input...")
 		select {
 		case <-game.shutdown:
 			return
-		case <-time.After(time.Millisecond * 10):
+		case <-time.After(time.Millisecond * 500):
 		}
+		game.writeMsg("drawing...")
+
+		drawCount++
+		for y := 0; y < BOARD_HEIGHT; y++ {
+			for x := 0; x < BOARD_WIDTH; x++ {
+				if (x+y+drawCount)%2 == 0 {
+					game.board.Cells[y][x].Color = tcell.NewRGBColor(255, 255, 255)
+				} else {
+					game.board.Cells[y][x].Color = tcell.NewRGBColor(0, 0, 0)
+				}
+			}
+		}
+		game.writeMsg(fmt.Sprintf("dc:%v", drawCount))
+
+		game.draw()
 	}
 }
 
@@ -53,4 +73,33 @@ func (game *Game) getInput() {
 			}
 		}
 	}
+}
+
+const BLOCK_CHAR = '\u2592' //
+
+func (game *Game) draw() {
+	// XXX: figure out how to center the board lol
+	charStyle := tcell.StyleDefault
+	for y := 0; y < BOARD_HEIGHT; y++ {
+		for x := 0; x < BOARD_WIDTH; x++ {
+			charStyle = charStyle.Background(game.board.Cells[y][x].Color)
+			game.screen.SetContent(x*2, y, BLOCK_CHAR, nil, charStyle)
+			game.screen.SetContent((x*2)+1, y, BLOCK_CHAR, nil, charStyle)
+		}
+	}
+	game.screen.Show()
+}
+
+func (game *Game) writeMsg(str string) {
+	y := BOARD_HEIGHT + 1
+
+	width, _ := game.screen.Size()
+	for x := 0; x < width; x++ {
+		game.screen.SetContent(x, y, '\u0000', nil, tcell.StyleDefault)
+	}
+
+	for x, ch := range str {
+		game.screen.SetContent(x, y, ch, nil, tcell.StyleDefault)
+	}
+	game.screen.Show()
 }
